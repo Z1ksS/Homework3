2da125911177a02671c39b272182dbf134e1c14d
Z1ksS <z1kuslovrov@gmail.com>
2023-04-28 11:41:22 +0300

    updated main.sql file

--- main.sql
+++ main.sql
@@ -1,4 +1,4 @@
---Створення таблиць
+--Creating tables
 CREATE TABLE Groups (
 	gr_id INT IDENTITY(1,1) PRIMARY KEY,
 	gr_name VARCHAR(50) NOT NULL,
@@ -21,17 +21,17 @@ CREATE TABLE Orders(
 	FOREIGN KEY (ord_an) REFERENCES Analysis(an_id)
 );
 
---Заповнення таблиць даними
+--Filling tables with data
 INSERT INTO Groups(gr_name, gr_temp) VALUES 
-	('Група A', 40.0), 
-	('Група Б', 45.0), 
-	('Група В', 35.0);
+	('Group A', 40.0), 
+	('Group B', 45.0), 
+	('Group C', 35.0);
 INSERT INTO Analysis (an_name, an_cost, an_price, an_group) VALUES 
-	('Аналіз 1', 100.0, 150.0, 1), 
-	('Аналіз 2', 200.0, 300.0, 1), 
-	('Аналіз 3', 50.0, 85.0, 2),
-	('Аналіз 4', 75.0, 90.0, 3),
-	('Аналіз 5', 150.0, 215.0, 3);
+	('Analysis 1', 100.0, 150.0, 1), 
+	('Analysis 2', 200.0, 300.0, 1), 
+	('Analysis 3', 50.0, 85.0, 2),
+	('Analysis 4', 75.0, 90.0, 3),
+	('Analysis 5', 150.0, 215.0, 3);
 INSERT INTO Orders (ord_datetime, ord_an) VALUES
   ('2020-02-04 10:00:00', 1),
   ('2020-02-05 11:00:00', 2),
