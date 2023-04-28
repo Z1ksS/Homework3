108904b782d866a789c14b95be159b6c2da6a7f1
Z1ksS <z1kuslovrov@gmail.com>
2023-04-28 11:48:25 +0300

    deleted folder task1

--- task1/task1.sql
+++
@@ -1,5 +0,0 @@
---Task 1
-SELECT an_name, an_price 
-FROM dbo.Analysis analys
-JOIN dbo.Orders ord ON analys.an_id = ord.ord_id
-WHERE ord.ord_datetime BETWEEN '2020-02-05' AND DATEADD(day, 7, '2020-02-05');
